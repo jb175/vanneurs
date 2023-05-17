967ee47593ff3a20208381cf9acabcc215e0997f
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2023-05-17 13:50:47 +0200

    change class to classsName

--- vanneurs-front/src/containers/NavBar/Navbar.js
+++ vanneurs-front/src/containers/NavBar/Navbar.js
@@ -1,28 +1,28 @@
 function NavBar() {
   return (
     <nav
-      class="navbar navbar-expand-lg navbar-light"
+      className="navbar navbar-expand-lg navbar-light"
       style={{ backgroundColor: "#335C81" }}
     >
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link text-white " href="/index">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link text-white " href="/index">
               Accueil
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="/profil">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/profil">
               Profil
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="/announcements">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/announcements">
               Annonces
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="/create-announcement">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/create-announcement">
               Créer une annonce
             </a>
           </li>
